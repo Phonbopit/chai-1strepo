41fd68508bbbccf435b29a30221860367d1d9ac6
Chai Phonbopit <Phonbopit@gmail.com>
2012-11-08 21:37:43 +0700

    Update src/github/firsttime/main/MainApp.java
    add comment

--- src/github/firsttime/main/MainApp.java
+++ src/github/firsttime/main/MainApp.java
@@ -6,15 +6,15 @@ import github.firsttime.system.*;
 public class MainApp {
 
 	public static void main(String[] args) {
-		
-		
+
 		Gameplay play = new Gameplay();
-	
+		//test initial 4 teams.
 		Team pool 		= new Team("Liverpool",  "English", "Barclay Premier League");
 		Team united	   	= new Team("ManUtd" ,    "English", "Barclay Premier League");
 		Team barca		= new Team("Barcelona",  "Spanish", "La Liga");
 		Team real		= new Team("Real Madrid","Spanish", "La Liga");
 		
+		//random 
 		play.initial(pool, united, 1);
 		play.initial(barca, real, 2);
 		play.initial(united, barca, 3);
